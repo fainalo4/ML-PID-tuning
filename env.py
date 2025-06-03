36cfd12506eef8f65f768afc47c3764e4cb54657
lorenzo <farinalorenzo4@gmail.com>
2025-06-03 10:56:06 +0200

    up stop condition and reward

--- env.py
+++ env.py
@@ -5,7 +5,8 @@ import jax
 from system import *
 
 class Env():
-    def __init__(self, system : SimpleDiscrete, states_size= 1, actions_size= 1):
+    def __init__(self, system : SimpleDiscrete, states_size= 1, actions_size= 1, T= 96):
+        
         self.system= system
 
         self.done = False
@@ -13,6 +14,7 @@ class Env():
 
         self.states_size= states_size
         self.actions_size= actions_size
+        self.T= T
 
         self.reward= 0
 
@@ -29,9 +31,12 @@ class Env():
 
         reward= self.rewards(x,u)
 
-        if self.system.t >=96: self.done= True
+        if self.system.t >=self.T: self.done= True
+        if self.system.x < 0 or self.system.x > 30: 
+            self.done= True
+            reward+= -1000
 
         return x_p, reward , self.done
     
     def rewards(self, x, u):
-        return -(self.system.x_t - x)**2 - u**2  
+        return -(self.system.x_t - x)**2 - 0.01*u**2  
