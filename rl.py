f21de20a4cb137049a3642145d479074780621ee
lorenzo <farinalorenzo4@gmail.com>
2025-07-02 14:58:19 +0200

    fix AC (SAC) for TD-error based update

--- rl.py
+++ rl.py
@@ -81,7 +81,7 @@ class Reinforce_w_baseline():
                                                       self.params_p,
                                                       step_p)
 
-class AC():
+class SAC():
     def __init__(self, env, policy, params_p, value, params_v, gamma, alpha_p, alpha_v):
         
         self.env = env
@@ -97,35 +97,48 @@ class AC():
     def train(self, num_episodes):
         for episode in tqdm(range(num_episodes), desc="episodes"):
 
-            states, actions, rewards= trajectory(self)
+            self.env.state, _= self.env.reset()
+            rewards = []
 
-            self.history_reward.append( sum(rewards) )
+            done = False
+            t=0 
+            while not done:
+                t+= 1
+                if verbose: print("t: ", t)
 
-            self.update_params(states, actions, rewards)
+                action = self.policy.sample_action(self.params_p, self.env.state)
+                next_state, reward, term, trunc, _ = self.env.step(action)
+                if verbose: print("state: ", self.env.state, "action: ", action, "reward: ", reward)
 
-    def update_params(self, states, actions, rewards):
-        for t in range(len(rewards[:-1])):
-            if verbose: print("t: ", t)
+                self.update_params(t, [self.env.state, next_state], action, reward)
 
-            hot_s= jnp.array(states[t], dtype=jnp.float32)
-            hot_s1= jnp.array(states[t+1], dtype=jnp.float32)
+                rewards.append(reward)
+                done= term or trunc
+                self.env.state= next_state
+                
+            self.history_reward.append(sum(rewards))
 
-            td_error= rewards[t] \
-                        + self.gamma* self.value.sample_value(self.params_v, hot_s1) \
-                        - self.value.sample_value(self.params_v, hot_s)
+    def update_params(self, t, states, action, reward):
+
+        hot_s= jnp.array(states[0], dtype=jnp.float32)
+        hot_s1= jnp.array(states[1], dtype=jnp.float32)
+
+        td_error= reward \
+                    + self.gamma* self.value.sample_value(self.params_v, hot_s1) \
+                    - self.value.sample_value(self.params_v, hot_s)
+        
+        step_v= self.alpha_v * td_error
+        self.params_v = self.value.update_value(hot_s,
+                                                self.params_v,
+                                                step_v)
+        
+        step_p= self.alpha_p * self.gamma**t * td_error
+        self.params_p = self.policy.update_policy(hot_s, 
+                                                    action,
+                                                    self.params_p,
+                                                    step_p)
             
-            step_v= self.alpha_v * td_error
-            self.params_v = self.value.update_value(hot_s,
-                                                    self.params_v,
-                                                    step_v)
-            
-            step_p= self.alpha_p * self.gamma**t * td_error
-            self.params_p = self.policy.update_policy(hot_s, 
-                                                      actions[t],
-                                                      self.params_p,
-                                                      step_p)
-            
-            
+# TODO: aggiungici advantage function A(s,a)...
 
 
 def trajectory(self):
@@ -154,5 +167,6 @@ def trajectory(self):
 
         self.env.state= next_state
         done= term or trunc
+        if done: states.append(next_state)
     
     return states, actions, rewards
