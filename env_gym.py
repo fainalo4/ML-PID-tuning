837b0f088d78f746a2c83b915f09ec425449eb04
lorenzo <farinalorenzo4@gmail.com>
2025-07-04 17:43:22 +0200

    add multi-D system as MultiDiscrete, env class generalized

--- env_gym.py
+++ env_gym.py
@@ -13,13 +13,13 @@ KEY= jax.random.PRNGKey(4)
 KEYS= hk.PRNGSequence(KEY)
 
 class Env(gym.Env):
-    def __init__(self, system : SimpleDiscrete, umin=0, umax=10, T= 96):
+    def __init__(self, system, o_dim, a_dim, umin, umax, T= 96):
 
         self.render_mode= None
 
-        self.observation_space= spaces.Box(-jnp.inf, jnp.inf, shape=(2,))
+        self.observation_space= spaces.Box(-jnp.inf, jnp.inf, shape=(2*o_dim,1))
         
-        self.action_space= spaces.Box(umin,umax, shape=())
+        self.action_space= spaces.Box(umin,umax, shape=(a_dim,1))
         # in case it can be changed after init
 
         self.system= system
@@ -40,7 +40,9 @@ class Env(gym.Env):
         # We need the following line to seed self.np_random
         super().reset(seed=seed)
 
-        self.system.x = self.system.x0 + jax.random.normal(next(KEYS))
+        self.system.x = self.system.x0 + jax.random.uniform(next(KEYS), 
+                                                            shape= self.system.x0.shape,
+                                                            minval=-5, maxval=5)
         self.system.v = self.random_disturbance()
         
         self.terminated = False
@@ -62,28 +64,30 @@ class Env(gym.Env):
 
         if self.system.t >= self.T: 
             self.terminated= True
-        if x_p < 0 or x_p > 30: 
+        if (x_p < 0).any() or (x_p > 30).any(): 
             self.truncated= True
             reward+= -1000
 
-        if verbose: print('o:', obs,'a', u, 'r:', reward )
+        if verbose: print('x', x_p, 'o:', obs,'a', u, 'r:', reward )
 
         return obs, reward, self.terminated, self.truncated, {}
     
     def rewards(self, x, u):
-        return -(self.system.x_t - x)**2 - 0.0001*u**2  
+        r_vec= -(self.system.x_t - x)**2 - 0.0001*u**2
+        return float(jnp.sum(r_vec))
     
     def observation(self, x):
         self.error= self.system.x_t - x
         self.error_integral+= self.error
-        return jnp.array([self.error, self.error_integral])
+        return jnp.concatenate([self.error, self.error_integral], axis=0)
     
 
     def random_disturbance(self):
-        v0 = jax.random.uniform(next(KEYS), minval= 15, maxval=20) 
+        v_dim= self.system.Bv.shape[1]
+        v0 = jax.random.uniform(next(KEYS), shape=(v_dim,1), minval= 15, maxval=20) 
         v=[v0]
         for t in range(self.T-1):
-            v.append(v[t] + jax.random.normal(next(KEYS), shape=()) * 0.5)
+            v.append(v[t] + jax.random.normal(next(KEYS), shape=(v_dim,1)) * 0.5)
         return v
 
 
--- system.py
+++ system.py
@@ -3,9 +3,9 @@ class SimpleDiscrete:
     def __init__(self, A, Bu, Bv, x0, x_t, v):
         """
         Initialize the environment with system matrices A, B and initial state x0.
-        A: State transition matrix (numpy array)
-        B: Control input matrix (numpy array)
-        x0: Initial state (numpy array). If None, initializes to zeros.
+        A: State transition matrix (scalar)
+        B: Control input matrix (scalar)
+        x0: Initial state (scalar). 
         """
         self.A = A
         self.Bu = Bu
@@ -22,9 +22,43 @@ class SimpleDiscrete:
     def step(self, u):
         """
         Apply control input u and update the state.
-        u: Control input (numpy array or scalar)
+        u: Control input (scalar)
         Returns the new state.
         """
         self.x = self.A * self.x + self.Bu * u + self.Bv * self.v[self.t]
+        self.t += 1
+        return self.x
+
+
+class MultiDiscrete:
+    def __init__(self, A, Bu, Bv, x0, x_t, v):
+        """
+        Initialize the environment with system matrices A, B and initial state x0.
+        A: State transition matrix (numpy array)
+        B: Control input matrix (numpy array)
+        x0: Initial state (numpy array)
+        x_t: Target state (numpy array)
+        v: Disturbance vector (numpy array)
+        """
+        self.A = A
+        self.Bu = Bu
+        self.Bv = Bv
+
+        self.x0 = x0
+        self.x = x0
+        self.x_t= x_t
+        self.v= v
+
+        self.t= 0
+
+
+    def step(self, u):
+        """
+        Apply control input u and update the state.
+        u: Control input (numpy array)
+        Returns the new state.
+        """
+        v_t= self.v[self.t].reshape(self.Bv.shape[1],1)
+        self.x = self.A @ self.x + self.Bu @ u + self.Bv @ v_t
         self.t += 1
         return self.x
