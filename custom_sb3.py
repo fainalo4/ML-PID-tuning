4041b88baa504d7061a55e9971c7d411150a17be
lorenzo <farinalorenzo4@gmail.com>
2025-07-07 18:04:43 +0200

    add custom NN as 3 different controllers

--- custom_sb3.py
+++ custom_sb3.py
@@ -1,9 +1,12 @@
 """
-https://stable-baselines3.readthedocs.io/en/master/guide/custom_policy.html
-Advanced Example
+Advanced Example for FA customization (SB3)
+- https://stable-baselines3.readthedocs.io/en/master/guide/custom_policy.html
+- https://stable-baselines3.readthedocs.io/en/master/_modules/stable_baselines3/common/distributions.html#CategoricalDistribution.proba_distribution_net
 
-https://stable-baselines3.readthedocs.io/en/master/_modules/stable_baselines3/common/distributions.html#CategoricalDistribution.proba_distribution_net
-Distributions
+Custom NN architecture (Pytorch): 
+- https://stackoverflow.com/questions/70269663/how-to-efficiently-implement-a-non-fully-connected-linear-layer-in-pytorch 
+- https://discuss.pytorch.org/t/parallel-execution-of-modules-in-nn-modulelist/43940/7 
+
 """
 
 from typing import Callable, Dict, List, Optional, Tuple, Type, Union
@@ -12,12 +15,23 @@ from gymnasium import spaces
 import torch as th
 from torch import nn
 import numpy as np
-from functools import partial
 
 from stable_baselines3.common.policies import ActorCriticPolicy
 from stable_baselines3.common.distributions import DiagGaussianDistribution
 from stable_baselines3.common.type_aliases import Schedule
 from stable_baselines3.common.preprocessing import get_action_dim
+
+
+class MultiNN(nn.Module):
+    def __init__(self, controllers_number) -> None:
+        super().__init__()
+        self.linears = nn.ModuleList([nn.Linear(2, 1, bias= False) for i in range(controllers_number)])
+
+    def forward(self, x : th.Tensor) -> th.Tensor:
+        y= th.Tensor()
+        for i,l in enumerate(self.linears):
+            y= th.cat([y, l(x[0][2*i:2*(i+1)]) ])
+        return y
 
 
 class CustomDistribution(DiagGaussianDistribution):
@@ -34,7 +48,7 @@ class CustomDistribution(DiagGaussianDistribution):
         :param log_std_init: Initial value for the log standard deviation
         :return:
         """
-        mean_actions = nn.Linear(latent_dim, self.action_dim, bias=False)
+        mean_actions = nn.Identity((latent_dim, self.action_dim))
         log_std = nn.Parameter(th.ones(self.action_dim) * log_std_init, requires_grad=True)
         return mean_actions, log_std
 
@@ -44,34 +58,37 @@ class CustomExtractor(nn.Module):
     Custom network for policy and value function.
     It receives as input the features extracted by the features extractor.
 
-    :param feature_dim: dimension of the features extracted with the features_extractor (e.g. features from a CNN)
-    :param last_layer_dim_pi: (int) number of units for the last layer of the policy network
+    :param observation_dim: dimension of the features extracted with the features_extractor (e.g. features from a CNN)
+    :param action_dim: (int) number of units for the last layer of the policy network
     :param last_layer_dim_vf: (int) number of units for the last layer of the value network
     """
 
     def __init__(
         self,
-        feature_dim: int = 2,
-        last_layer_dim_pi: int = 1,
-        last_layer_dim_vf: int = 1,
+        observation_dim: int,
+        action_dim: int,
+        value_dim: int = 1,
     ):
         super().__init__()
 
         # IMPORTANT:
         # Save output dimensions, used to create the distributions
-        self.latent_dim_pi = last_layer_dim_pi
-        self.latent_dim_vf = last_layer_dim_vf
+        self.latent_dim_pi = action_dim
+        self.latent_dim_vf = value_dim
+        self.obs_dim= observation_dim
 
         # Policy network
-        self.policy_net = nn.Sequential(
-            nn.Linear(feature_dim, 1, bias=False)
-            )
+        self.policy_net =  nn.Linear(in_features= observation_dim,
+                      out_features= action_dim,
+                      bias= False)
+
+        # self.policy_net= MultiNN(controllers_number= self.obs_dim//2)
         
         # Value network
         self.value_net = nn.Sequential(
-            nn.Linear(feature_dim, 4), nn.ReLU(),
+            nn.Linear(self.obs_dim, 4), nn.ReLU(),
             nn.Linear(4,2), nn.ReLU(),
-            nn.Linear(2,1)
+            nn.Linear(2,value_dim)
         )
 
     def forward(self, features: th.Tensor) -> Tuple[th.Tensor, th.Tensor]:
@@ -82,7 +99,7 @@ class CustomExtractor(nn.Module):
         return self.forward_actor(features), self.forward_critic(features)
 
     def forward_actor(self, features: th.Tensor) -> th.Tensor:
-        return self.policy_net(features)
+        return self.policy_net(features) 
 
     def forward_critic(self, features: th.Tensor) -> th.Tensor:
         return self.value_net(features)
@@ -97,20 +114,24 @@ class CustomActorCriticPolicy(ActorCriticPolicy):
         *args,
         **kwargs,
     ):
+        
+        self.obs_dim= observation_space.shape[0]      # type: ignore
+        self.act_dim= action_space.shape[0]      # type: ignore
+        
         # Disable orthogonal initialization
         kwargs["ortho_init"] = False
         super().__init__(
             observation_space,
             action_space,
             lr_schedule,
-            # Pass remaining arguments to base class
             *args,
             **kwargs,
         )
 
-
     def _build_mlp_extractor(self) -> None:
-        self.mlp_extractor = CustomExtractor(self.features_dim)
+        self.mlp_extractor = CustomExtractor(self.obs_dim,
+                                             self.act_dim)
+
 
     def _build(self, lr_schedule: Schedule) -> None:
         """
