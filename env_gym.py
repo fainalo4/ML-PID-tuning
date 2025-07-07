90e882e22187b136b0572cfffae1addcf5cac374
lorenzo <farinalorenzo4@gmail.com>
2025-07-07 14:51:15 +0200

    jnp moved to np dependencies

--- env_gym.py
+++ env_gym.py
@@ -1,7 +1,4 @@
-import jax.numpy as jnp
-import jax
-import haiku as hk
-
+import numpy as np
 import gymnasium as gym
 from gymnasium import spaces
 
@@ -9,17 +6,15 @@ from system import *
 
 verbose= False
 
-KEY= jax.random.PRNGKey(4)
-KEYS= hk.PRNGSequence(KEY)
 
 class Env(gym.Env):
     def __init__(self, system, o_dim, a_dim, umin, umax, T= 96):
 
         self.render_mode= None
 
-        self.observation_space= spaces.Box(-jnp.inf, jnp.inf, shape=(2*o_dim,1))
+        self.observation_space= spaces.Box(-np.inf, np.inf, shape=(2*o_dim,1))
         
-        self.action_space= spaces.Box(umin,umax, shape=(a_dim,1))
+        self.action_space= spaces.Box(np.float32(umin), np.float32(umax), shape=(a_dim,1))
         # in case it can be changed after init
 
         self.system= system
@@ -40,9 +35,8 @@ class Env(gym.Env):
         # We need the following line to seed self.np_random
         super().reset(seed=seed)
 
-        self.system.x = self.system.x0 + jax.random.uniform(next(KEYS), 
-                                                            shape= self.system.x0.shape,
-                                                            minval=-5, maxval=5)
+        self.system.x = self.system.x0 + np.random.uniform(low=-5, high=5,
+                                                           size= self.system.x0.shape)
         self.system.v = self.random_disturbance()
         
         self.terminated = False
@@ -74,20 +68,20 @@ class Env(gym.Env):
     
     def rewards(self, x, u):
         r_vec= -(self.system.x_t - x)**2 - 0.0001*u**2
-        return float(jnp.sum(r_vec))
+        return float(np.sum(r_vec))
     
     def observation(self, x):
         self.error= self.system.x_t - x
         self.error_integral+= self.error
-        return jnp.concatenate([self.error, self.error_integral], axis=0)
+        return np.concatenate([self.error, self.error_integral], axis=0)
     
 
     def random_disturbance(self):
         v_dim= self.system.Bv.shape[1]
-        v0 = jax.random.uniform(next(KEYS), shape=(v_dim,1), minval= 15, maxval=20) 
+        v0 = np.random.uniform(low= 15, high=20, size=(v_dim,1)) 
         v=[v0]
         for t in range(self.T-1):
-            v.append(v[t] + jax.random.normal(next(KEYS), shape=(v_dim,1)) * 0.5)
+            v.append(v[t] + np.random.normal(size=(v_dim,1)) * 0.5)
         return v
 
 
